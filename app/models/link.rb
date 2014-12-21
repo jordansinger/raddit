1afc8143062df6f9e48ee2389e03bfb0f6ac503f
Jordan Singer <singer.jordan@gmail.com>
2014-12-21 12:10:01 -0700

    automatically suggest url title

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,12 @@ class Link < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
+	after_create :set_title
+
+	def set_title
+		url = self.url
+		title = Mechanize.new.get(url).title
+		self.title = title
+		self.save
+	end
 end
